6f49302498fc6fa04c8e82d6369888d57aa572b4
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-13 20:42:17 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -14,10 +14,10 @@ def curfew_checker(time)
 end
 
 def complex_curfew_checker(time)
-  if time = 11
-    "Time to apparate!"
-  elsif time > 11
+  if time > 11
     "You're in trouble! Better get home quick!"
+  elsif time = 11
+    "Time to apparate!"
   end
 end
 
