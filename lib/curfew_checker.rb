67745d9bf3cb3eb21ca3413d83b885241f1c3233
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-13 20:51:56 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -37,7 +37,7 @@ end
 def platinum_curfew_checker(current_time, curfew_time)
   if curfew_time == curfew_time
     "Time to apparate!"
-  elsif curfew_time > 11
+  elsif curfew_time > curfew_tme
     "You're in trouble! Better get back to Hogwarts quick!"
   end
 end
